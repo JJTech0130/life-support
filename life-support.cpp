35acf6c0929dcc0d179866e5ba9b4b28ec225c54
JJTech0130 <jjtech@jjtech.dev>
2024-05-27 18:41:18 -0400

    clean up

--- life-support.cpp
+++ life-support.cpp
@@ -2,24 +2,11 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 
-#include "blink.pio.h"
 #include "hdq.pio.h"
 #include "hardware/clocks.h"
 
 #define HDQ_PIN 14
 #define DFU_PIN 15
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-    
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
 
 void hdq_slave_init(PIO pio, uint sm, uint offset, uint pin) {
     gpio_pull_up(pin);
@@ -30,17 +17,12 @@ void hdq_slave_init(PIO pio, uint sm, uint offset, uint pin) {
 
 void hdq_slave_put_byte(PIO pio, uint sm, bool valid, uint8_t data) {
     uint32_t hdq_w = (data << 1) | valid; // valid bit is least significant
-    // if (valid) {
-    //     //hdq_w = 0xFFFFFFFF;
-    //     //hdq_w = 0x00000001;
-    // }
     pio_sm_put_blocking(pio, sm, hdq_w);
+
     // Wait for the FIFO to empty
-    //printf("Waiting for FIFO to empty\n");
     while (!pio_sm_is_tx_fifo_empty(pio, sm)) {
         tight_loop_contents();
     }
-    //printf("FIFO empty\n");
 }
 
 uint8_t hdq_slave_get_byte(PIO pio, uint sm) {
@@ -71,27 +53,17 @@ void hdq_slave_handle(PIO pio, uint sm) {
             printf("HDQ read:  0x%02x\n", addr);
             hdq_slave_put_byte(pio, sm, true, addr); // echo the addr
         }
-        // Print the FIFO size
-        printf("FIFO: %d\n", pio_sm_get_tx_fifo_level(pio, sm));
     }
 }
 
 int main() {
     stdio_init_all();
 
-    //sleep_ms(3000); // delay for 3 seconds
 
-    // PIO Blinking example
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &hdq_slave_program);
     printf("Loaded program at %d\n", offset);
     
     hdq_slave_init(pio, 0, offset, HDQ_PIN);
-    //pio_sm_set_enabled(pio, 0, true);
-    printf("HDQ Slave enabled\n");
-
     hdq_slave_handle(pio, 0);
-   
-    // Stop pio
-    pio_sm_set_enabled(pio, 0, false);
 }
