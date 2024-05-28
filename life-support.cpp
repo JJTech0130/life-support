a4c704577cdadc0a135bf1384acb70812e8f1543
JJTech0130 <jjtech@jjtech.dev>
2024-05-28 08:28:29 -0400

    use calloc, provide mV

--- life-support.cpp
+++ life-support.cpp
@@ -15,15 +15,8 @@ int main() {
     printf("Loaded program at %d\n", offset);
     
     hdq_slave_init(pio, 0, offset, HDQ_PIN);
-    uint16_t *register_mem = (uint16_t *)malloc(256);
-    uint16_t *control_mem = (uint16_t *)malloc(0x90);
-    // Write zeros to memory
-    for (int i = 0; i < 256; i++) {
-        register_mem[i] = 0;
-    }
-    for (int i = 0; i < 0x90; i++) {
-        control_mem[i] = 0;
-    }
+    uint16_t *register_mem = (uint16_t *)calloc(256, sizeof(uint16_t));
+    uint16_t *control_mem = (uint16_t *)malloc(0x90 * sizeof(uint16_t));
 
     control_mem[2] = 0x0106; // Version    
     register_mem[0x6/2] = register_mem[0x28/2] = 3017; // Temp: 301.8K = 82.13F
@@ -33,6 +26,7 @@ int main() {
     register_mem[0x10/2] = register_mem[0x20/2] = register_mem[0x22/2] = 1629; // Remaining capacity mAh
     register_mem[0x14/2] = 0; // Average current mA
     register_mem[0x2C/2] = 100; // State of charge
+    register_mem[0x08/2] = 4300; // Voltage mV
 
     hdq_slave_handle(pio, 0, register_mem, control_mem);
 }
